858b062b7d674b1d9cd9c060728aea6b5746db72
John Shenk <john_shenk@hotmail.com>
2020-07-13 08:15:34 -0500

    switch to wss

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const api = process.env.NODE_ENV === 'production' ? 'https://differencebetween.herokuapp.com' : 'http://localhost:7000';
-const ws = process.env.NODE_ENV === 'production' ? 'ws://differencebetween.herokuapp.com' : 'ws://localhost:7000';
+const ws = process.env.NODE_ENV === 'production' ? 'wss://differencebetween.herokuapp.com' : 'ws://localhost:7000';
 
 export {
 	api,
